30e2e4608b7595840ce289b37cfed79ecae098c0
aterminel <terminelal@gmail.com>
2016-06-16 17:26:09 -0400

    "Ruby code in view moved to controll"

--- app/controllers/greeter_controller.rb
+++ app/controllers/greeter_controller.rb
@@ -1,4 +1,11 @@
 class GreeterController < ApplicationController
   def hello
+  random_names = ["Alex", "John", "Joe"]
+  @names=random_names.sample
+  @time = Time.now
+  @times_displayed ||= 0
+  @times_displayed += 1
+  end
+  def goodybye
   end
 end
